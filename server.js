f2bde309b3b2768dc83f07fe48848e002c775f2c
Patrick Hilmoe <pshilmoe@gmail.com>
2024-05-25 11:06:52 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+// app.use((req, res) => {
+//     console.log(req.headers);
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+// });
+
+app.get('/', (req,res) => {
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+})
+
+app.get('/message', (req,res) => {
+    res.render('index')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
