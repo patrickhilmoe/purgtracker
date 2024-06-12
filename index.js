c50ab81de57ec174fea31291b1140993cebae9f9
patrickhilmoe <pshilmoe@gmail.com>
2024-06-11 21:39:36 -0700

    added user model

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const mongoose = require('mongoose');
 const listRoutes = require('./routes/lists');
 const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./model/user');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,6 +15,16 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+
+app.use(session())
+app.use(passport.initialize());
+// passport.session allows login to persist across pages
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser())
 
 const sessionOptions = {secret: 'thisisnotagoodsecret', resave: false, saveUninitialized: false}
 app.use(session(sessionOptions));
